31721cfd6c2be8a5676757fb93dbbcce90235ebf
jb3rns <jb3rns@gmail.com>
2019-05-08 14:58:19 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-var correctGuess = false;
+var correctGuess = false; 
 var randNum = Math.floor(Math.random() * 6) + 1;
+
+// prompts the user to type in a guess
 var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
+
+// if input is correct set correctGuess to true and disregard the preceding else ifs, goes to next if statement
 if(parseInt(guess) === randNum) {
   correctGuess = true;
-} else if (parseInt(guess) < randNum) {
+} 
+// if the user's first guess is too low, prompt him/her for a second time
+else if (parseInt(guess) < randNum) {
   var guessMore = prompt('Try again. The number I am thinking of is more than ' + guess + '.');
+  // if the user guesses correctly set correctGuess to true
   if (parseInt(guessMore) === randNum) {
     correctGuess = true;
   }
-} else if (parseInt(guess) > randNum) {
+} 
+
+// if the user's first guess is too high, prompt him/her for a second time
+else if (parseInt(guess) > randNum) {
   var guessLess = prompt('Try again. The number I am thinking of is less than ' + guess + '.');
+  // if the user guesses correctly set correctGuess to true
   if (parseInt(guessLess) === randNum) {
     correctGuess = true;
   }
 }
+// tell the user he/she guessed correctly
 if(correctGuess) {
   document.write("<p>You guessed it right!</p>");
-} else {
+}
+// wrong guess 
+else {
   document.write("<p>Sorry. The number is " + randNum + ".</p>");
 }
