76b8c9d261a1a4f7db091317f444b929f54c8880
jb3rns <jb3rns@gmail.com>
2019-05-08 14:48:05 -0500

    Added if else clauses

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+var correctGuess = false;
 var randNum = Math.floor(Math.random() * 6) + 1;
 var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
 if(parseInt(guess) === randNum) {
+  correctGuess = true;
+} else if (parseInt(guess) < randNum) {
+  var guessMore = prompt('Try again. The number I am thinking of is more than ' + guess + '.');
+  if (parseInt(guessMore) === randNum) {
+    correctGuess = true;
+  }
+} else if (parseInt(guess) > randNum) {
+  var guessLess = prompt('Try again. The number I am thinking of is less than ' + guess + '.');
+  if (parseInt(guessLess) === randNum) {
+    correctGuess = true;
+  }
+}
+if(correctGuess) {
   document.write("<p>You guessed it right!</p>");
 } else {
   document.write("<p>Sorry. The number is " + randNum + ".</p>");
